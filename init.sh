cad6a7aec0c6279136cd720d732cab4eb9400dab
varnikgen <1Nhfycbkmdfybz!>
2020-02-13 11:38:52 +1000

    init.d modifed

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+
+sudo rm -rf /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
 
