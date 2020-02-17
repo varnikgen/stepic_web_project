76a6d038ff278607fbb47f221cae568770554ff4
varnikgen <1Nhfycbkmdfybz!>
2020-02-17 14:44:26 +1000

    edit

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-def wsgi_application(environ, start_response):
+def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
--- init.sh
+++ init.sh
@@ -1,11 +1,6 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-
-sudo rm -rf /etc/nginx/sites-enabled/default
-
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
-
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
-
-﻿sudo /etc/init.d/mysql start﻿
