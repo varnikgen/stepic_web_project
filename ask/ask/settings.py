14ca2f169835d6f77a59a0884fdc883a4c7476df
varnikgen <1Nhfycbkmdfybz!>
2020-02-21 07:55:46 +1000

    edit configuration nginx project (settings.py)

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '806vhf)l^n5@3wz8k%yel)y+&^^ip8$d-@*(th2$&j72yxzhk9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
