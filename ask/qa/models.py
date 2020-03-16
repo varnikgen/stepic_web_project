5367dd8356c322c2f44d83f770609791ad340e9f
Nikolay <mellonkhv@gmail.com>
2020-03-17 00:16:17 +1000

    edit QuestionManager and Question classes

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 # Create your models here.
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(default="", max_length=255)
     text = models.TextField(default="")
     added_at = models.DateTimeField(auto_now_add=True)
@@ -13,14 +22,6 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
 
 
 class Answer(models.Model):
--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/django-gunicorn.conf /etc/gunicorn.d/test-django
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+#sudo ln -sf /home/box/web/etc/django-gunicorn.conf /etc/gunicorn.d/test-django
+#sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 #git clone https://github.com/varnikgen/stepic_web_project.git web
